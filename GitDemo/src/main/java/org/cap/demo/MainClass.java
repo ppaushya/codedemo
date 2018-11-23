34a70ba720a41e593f9426412e8c19f3e16611a2
ketchauh <ketchauh@DIN73002751.corp.capgemini.com>
2018-11-23 12:27:21 +0530

    First commit.

---
+++ GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,15 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World in Git!!!");
+		greeting();
+	}
+
+	
+	public static void greeting() {
+		System.out.println("Have a nice day!");
+	}
+}
